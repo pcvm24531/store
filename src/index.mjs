5718c4c6501a25af11c0887552211be8b78203ee
Cesar <csr2453@outlook.es>
2024-05-20 17:02:15 -0400

    Configurarción sin el método buscar puerto

--- src/index.mjs
+++ src/index.mjs
@@ -1,31 +1,70 @@
 import express from "express";
-import 'dotenv/config';
-import {findAvailablePosrt} from './utils/port.mjs';
-import mongoose from "mongoose";
 import { mockUsers } from "./utils/constants.mjs";
+import "dotenv/config"
 
 const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Runnin on port ${PORT}`);
+})
 
-app.use(express.json);
+//Ejemplo con query paramas
+app.get(
+    '/v0/users',
+    (request, response)=>{
+        console.log(request.query);
+        const  {
+            query: {filter, value}
+        }=request;
+        if(filter && value) return response.send(
+                mockUsers.filter( (user)=>user[filter].includes(value) )
+            );        
+        return response.send(mockUsers);
+    }
+);
 
-const desiredPort = process.env.PORT ?? 3000;
-const db_conn = process.env.DB_URI;
+app.get(
+    '/v0/users/:id',
+    (request, response)=>{
+        const paramID = parseInt( request.params.id );
+        if (isNaN(paramID)) return response.status(400).send({msg:"Bad request"})
+        
+        const findUser = mockUsers.find( (user)=>user.id===paramID );
+        if( !findUser ) return response.status(400).send({msg:"User not foud"})
 
-mongoose.connect( db_conn )
-    .then( ()=>console.log('DB Connected!!') )
-    .catch( (err)=>console.log(`Error:${err}`) );
+        return response.status(201).send(findUser);
+    }
+);
 
-findAvailablePosrt(desiredPort)
-    .then(port=>{
-        app.listen(port, ()=>{
-            console.log(`Server listen on http://localhost:${port}`);
-        })
-    });
-
-app.get( 
+app.post(
     '/v0/users',
     ( request, response )=>{
-        console.log(`Llamada a la ruta: /v0/users`);
-        return response.status(200).send( mockUsers );
+        console.log(request.body);
+        const { body } = request;
+        const newUser = { id:mockUsers[mockUsers.length-1].id+1, ...body };
+        mockUsers.push(newUser);
+        return response.status(200).send(newUser);
     }
- );
+);
+
+app.put(
+    '/v0/users/:id',
+    (request, response)=>{
+        const { 
+            body, 
+            params:{id} 
+        } = request;
+
+        const paramID = parseInt(id);
+        if( isNaN(paramID) ) return response.sendStatus(400);
+
+        const findUserIndex = mockUsers.findIndex( (user)=>user.id===paramID );;
+
+        if(findUserIndex===-1) return response.sendStatus(404);
+
+        mockUsers[findUserIndex] = { id:paramID, ...body };
+
+        return response.sendStatus(200);
+    }
+);
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,11 +1,4 @@
 export const mockUsers = [
-    {"id":1,"name":"Ricardo","username":"ricki","password":"Abc123"},
-    {"id":2,"name":"Lucas","username":"luc","password":"Abc123"},
-    {"id":3,"name":"Roger","username":"rog","password":"Abc123"},
-];
-
-export const mockProducst = [
-    {"name":"Paracetamol", "price":"12.5"},
-    {"name":"Mentizan", "price":"5"},
-    {"name":"Amoxi", "price":"7"},
+    {'id':1, "username":"pcvm", "password":"Abcde123"},
+    {'id':2, "username":"csr", "password":"Abcde123"},
 ];
