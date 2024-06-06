9a38f85709e7553bc9c644c49590977d332094a4
Cesar <csr2453@outlook.es>
2024-06-06 17:01:25 -0400

    Se agregó reglas para products

--- src/utils/validationSchemas.mjs
+++ src/utils/validationSchemas.mjs
@@ -1,19 +1,76 @@
 export const createUserValidationSchema = {
-    username:{
+    name:{
         isLength:{
-            options:{min:5, max:32},
-            errorMessage:"Userbame must be at least 5 characters with a max of 32 characters",
+            options:{min:3, max:25},
+            errorMessage:"Este campo debe contener entre 3 - 25 caracteres."
         },
         notEmpty:{
-            errorMessage:"Username cannot be empty",
+            errorMessage:"Este campo no puede estar vacío."
         },
-        isString: {
-            errorMessage:"Username must be a string!",
+        isString:{
+            errorMessage:"El nombre solo puede contener letras."
+        }
+    },
+    lastName:{
+        isLength:{
+            options:{min:3, max:25},
+            errorMessage:"Este campo debe contener entre 3 - 25 caracteres.",
+        },
+        notEmpty:{
+            errorMessage:"Este campo no puede estar vacío."
+        },
+        isString:{
+            errorMessage:"El apellido solo puede conter letras."
         },
     },
-    displayName:{
+    ci:{
+        isLength:{
+            options:{min:5, max:15},
+            errorMessage:"Este campo debe contener entre 5 - 15 caracteres."
+        },
+    },
+    username:{
+        isLength:{
+            options:{min:5, max:15},
+            errorMessage:"Este campo debe contener entre 5 - 15 caracteres.",
+        },
         notEmpty:{
-            errorMessage:"This field cannot empty!"
+            errorMessage:"Este campo no puede estar vacío.",
+        },
+    },
+    password:{
+        isLength:{
+            options:{min:5, max:15},
+            errorMessage:"Este campo de contener entre 5 - 25 caracateres."
+        },
+        notEmpty:{
+            errorMessage:"Este campo no puede estar vácio."
+        }
+    },
+    phone:{
+        isLength:{
+            options:{min:7, max:8},
+            errorMessage:"Ingrese un número válido."
+        },
+    },
+    position:{
+        notEmpty:{
+            errorMessage:"Este campo no puede estar vacío."
+        },
+    },
+    dateOfContract:{
+        notEmpty:{
+            errorMessage:"Este campo no puede estar vacío."
+        }
+    },
+    workingHours:{
+        notEmpty:{
+            errorMessage:"Este campo puede estar vacío."
+        }
+    },
+    supervisor:{
+        notEmpty:{
+            errorMessage:"Este campo no puede estar vacío."
         }
     }
 }
