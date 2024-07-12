4aa0756ef766e4ac521d572b99d7cae80739b0e9
Pablo HP <pcvm2453@gmail.com>
2024-07-11 22:34:53 -0400

    Add method status()

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -12,14 +12,24 @@ router.post(
                 password
             } 
         } = request;
-        //const findUser = await User.find( (user)=> user.userName===username );
-        const users = await User.find();
+        const users = await User.find();console.log(users);
         const findUser = users.find( (user)=>user.userName===username );
-        console.log(findUser);
+        
         if( !findUser || findUser.password !== password) return response.status(401).send({msg:"Bad credential"});
 
         request.session.user = findUser;
         return response.status(200).send(findUser);
+    }
+);
+router.get(
+    "/v0/auth/status",
+    (request, response)=>{
+        request.sessionStore.get(request.sessionID, (err, session)=>{
+            console.log(session);
+        });
+        return request.session.user 
+        ? response.status(200).send(request.session.user) 
+        : response.status(401).send({msg:"Not Authenticated"});
     }
 );
 export default router;
